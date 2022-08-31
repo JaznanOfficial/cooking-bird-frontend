5fd115288911229955d76492a6d42f969fc51d92
JaznanOfficial <jaznanofficial@gmail.com>
2022-08-31 18:54:59 +0600

    all cleaning

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>welcome to cooking bird development branch</h1>
-      <h3>this is from abdullah</h3>
-      <h1>hello i'm Abdullah.</h1>
-    </div>
-  );
+  return <div className="App">
+  
+  </div>;
 }
 
 export default App;
