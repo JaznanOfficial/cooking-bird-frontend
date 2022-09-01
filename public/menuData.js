f58def21140694bb7f8bd51b6ca046ef9ae74bf8
JaznanOfficial <jaznanofficial@gmail.com>
2022-09-01 20:46:22 +0600

    menu data making

---
+++ public/menuData.js
@@ -0,0 +1,195 @@
+const menuData = [
+    {
+        name: "Cold Falooda",
+        category: "Dessert",
+        area: "Bangladesh",
+        items: "rose syrup, vermicelli, sweet basil seeds, milk,ice etc",
+        img: 'https://www.cookwithkushi.com/wp-content/uploads/2019/06/IMG_3859_.jpg',
+        rating:''
+    },
+    {
+        name: "Misti Doi",
+        category: "Dessert",
+        area: "Bangladesh",
+        items: "milk, curd culture and jaggery or sugar",
+        img: 'https://www.spiceupthecurry.com/wp-content/uploads/2015/12/homemade-yogurt-recipe-7.jpg',
+        rating:''
+    },
+    {
+        name: "Chocolate Ice Cream",
+        category: "Dessert",
+        area: "worldwide",
+        items: "chocolate, ice cream,others",
+        img: 'https://templates.hibootstrap.com/restant/default/assets/img/home-one/collection/7.jpg',
+        rating:''
+    },
+    {
+        name: "Doughnut Chocolate",
+        category: "Dessert",
+        area: "worldwide",
+        items: "butter, milk, corn syrup, and vanilla",
+        img: 'https://templates.hibootstrap.com/restant/default/assets/img/home-one/collection/3.jpg',
+        rating:''
+    },
+    {
+        name: "Dark Chocolate Cake",
+        category: "Dessert",
+        area: "worldwide",
+        items: "chocolate, ice cream,cake",
+        img: 'https://templates.hibootstrap.com/restant/default/assets/img/home-one/collection/1.jpg',
+        rating:''
+    },
+    
+    {
+        name: "Dark Chocolate Cake",
+        category: "Dessert",
+        area: "worldwide",
+        items: "chocolate, ice cream,cake",
+        img: 'https://templates.hibootstrap.com/restant/default/assets/img/home-one/collection/4.jpg',
+        rating:''
+    },
+    {
+        name: "Hamburger",
+        category: "Fast Food",
+        area: "German",
+        items: "beef, cheese, lettuce, tomato, onion, pickles, bacon, or chilies",
+        img: 'https://elanta.app/nazar/starbelly-demo/img/menu/3.jpg',
+        rating:''
+    },
+    {
+        name: "Pizza xl",
+        category: "Fast Food",
+        area: "Italian",
+        items: "bread dough topped with some combination of olive oil, oregano, tomato, olives, mozzarella or other cheese, and many other ingredients",
+        img: 'https://static.toiimg.com/photo/msid-87930581/87930581.jpg?211826',
+        rating:''
+    },
+    {
+        name: "Spicy Hot Dog",
+        category: "Fast Food",
+        area: "worldwide",
+        items: "chocolate, ice cream,cake",
+        img: 'https://realfood.tesco.com/media/images/SpicedAubergineHotDog-LGH-v2-fde94345-16f3-459f-916e-b5fe2b6e9043-0-1400x919.jpg',
+        rating:''
+    },
+    {
+        name: "Dark Chocolate Cake",
+        category: "Dessert",
+        area: "worldwide",
+        items: "chocolate, ice cream,cake",
+        img: 'https://templates.hibootstrap.com/restant/default/assets/img/home-one/collection/1.jpg',
+        rating:''
+    },
+    {
+        name: "Dark Chocolate Cake",
+        category: "Dessert",
+        area: "worldwide",
+        items: "chocolate, ice cream,cake",
+        img: 'https://templates.hibootstrap.com/restant/default/assets/img/home-one/collection/1.jpg',
+        rating:''
+    },
+    {
+        name: "Dark Chocolate Cake",
+        category: "Dessert",
+        area: "worldwide",
+        items: "chocolate, ice cream,cake",
+        img: 'https://templates.hibootstrap.com/restant/default/assets/img/home-one/collection/1.jpg',
+        rating:''
+    },
+    {
+        name: "Dark Chocolate Cake",
+        category: "Dessert",
+        area: "worldwide",
+        items: "chocolate, ice cream,cake",
+        img: 'https://templates.hibootstrap.com/restant/default/assets/img/home-one/collection/1.jpg',
+        rating:''
+    },
+    {
+        name: "Dark Chocolate Cake",
+        category: "Dessert",
+        area: "worldwide",
+        items: "chocolate, ice cream,cake",
+        img: 'https://templates.hibootstrap.com/restant/default/assets/img/home-one/collection/1.jpg',
+        rating:''
+    },
+    {
+        name: "Dark Chocolate Cake",
+        category: "Dessert",
+        area: "worldwide",
+        items: "chocolate, ice cream,cake",
+        img: 'https://templates.hibootstrap.com/restant/default/assets/img/home-one/collection/1.jpg',
+        rating:''
+    },
+    {
+        name: "Dark Chocolate Cake",
+        category: "Dessert",
+        area: "worldwide",
+        items: "chocolate, ice cream,cake",
+        img: 'https://templates.hibootstrap.com/restant/default/assets/img/home-one/collection/1.jpg',
+        rating:''
+    },
+    {
+        name: "Dark Chocolate Cake",
+        category: "Dessert",
+        area: "worldwide",
+        items: "chocolate, ice cream,cake",
+        img: 'https://templates.hibootstrap.com/restant/default/assets/img/home-one/collection/1.jpg',
+        rating:''
+    },
+    {
+        name: "Dark Chocolate Cake",
+        category: "Dessert",
+        area: "worldwide",
+        items: "chocolate, ice cream,cake",
+        img: 'https://templates.hibootstrap.com/restant/default/assets/img/home-one/collection/1.jpg',
+        rating:''
+    },
+    {
+        name: "Dark Chocolate Cake",
+        category: "Dessert",
+        area: "worldwide",
+        items: "chocolate, ice cream,cake",
+        img: 'https://templates.hibootstrap.com/restant/default/assets/img/home-one/collection/1.jpg',
+        rating:''
+    },
+    {
+        name: "Dark Chocolate Cake",
+        category: "Dessert",
+        area: "worldwide",
+        items: "chocolate, ice cream,cake",
+        img: 'https://templates.hibootstrap.com/restant/default/assets/img/home-one/collection/1.jpg',
+        rating:''
+    },
+    {
+        name: "Dark Chocolate Cake",
+        category: "Dessert",
+        area: "worldwide",
+        items: "chocolate, ice cream,cake",
+        img: 'https://templates.hibootstrap.com/restant/default/assets/img/home-one/collection/1.jpg',
+        rating:''
+    },
+    {
+        name: "Dark Chocolate Cake",
+        category: "Dessert",
+        area: "worldwide",
+        items: "chocolate, ice cream,cake",
+        img: 'https://templates.hibootstrap.com/restant/default/assets/img/home-one/collection/1.jpg',
+        rating:''
+    },
+    {
+        name: "Dark Chocolate Cake",
+        category: "Dessert",
+        area: "worldwide",
+        items: "chocolate, ice cream,cake",
+        img: 'https://templates.hibootstrap.com/restant/default/assets/img/home-one/collection/1.jpg',
+        rating:''
+    },
+    {
+        name: "Dark Chocolate Cake",
+        category: "Dessert",
+        area: "worldwide",
+        items: "chocolate, ice cream,cake",
+        img: 'https://templates.hibootstrap.com/restant/default/assets/img/home-one/collection/1.jpg',
+        rating:''
+    },
+];
