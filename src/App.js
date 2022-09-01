addfa19af74cec0f91c995fcedd904fa4f610ee4
Nijumdip <nijumdip019911@gmail.com>
2022-09-01 23:53:30 +0600

    navber added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from "./logo.svg";
 import "./App.css";
+import HomePage from "./Pages/Home/HomePage/HomePage";
 
 function App() {
 
   return <div className="App">
-    <h1>Welcome to COOKING BIRD site!</h1>
+    <HomePage />
   </div>
 
 
