f3f75bb67d8ce5c2992a93553b901ac5a48944d8
Nijumdip <nijumdip019911@gmail.com>
2022-09-09 20:17:42 +0600

    add a carousel

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,33 +1,31 @@
 import React from "react";
+import "./Banner";
+import Ben2 from "./Ben2";
 
 const Banner = () => {
   return (
-   <div className="bg-base-200 my-5">
-       <div className="container flex flex-wrap justify-around items-center lg:flex-row-reverse py-5">
+    <div className="bg-base-200 my-5">
+      <div className="container relative flex flex-wrap justify-around items-center lg:flex-row-reverse py-5">
+        
         <div>
-          <img
-            src="https://placeimg.com/260/400/arch"
-            className="max-w-sm rounded-lg shadow-2xl"
-            alt=""
-          />
+         <Ben2 />
         </div>
-        <div className="text-center ">
+
+        <div className="text-start py-4 px-3 description-div">
           <h1 className=" text-5xl font-bold">Box Office News!</h1>
-          <p className="py-6 px-3 text-start">
+          <p className="py-6">
             Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda{" "}
             <br />
             excepturi exercitationem quasi. In deleniti eaque aut repudiandae et{" "}
             <br />a id nisi.
           </p>
-          <button className="ml-3 inline-flex items-center justify-center whitespace-nowrap rounded-md border border-transparent bg-red-500 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-red-600">Get Started</button>
-        
+          <button className=" inline-flex items-center justify-center whitespace-nowrap rounded-md border border-transparent bg-red-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-red-500">
+            Get Started
+          </button>
         </div>
-      </div> 
-      
- 
-    </div> 
-
-    
+        
+      </div>
+    </div>
   );
 };
 
