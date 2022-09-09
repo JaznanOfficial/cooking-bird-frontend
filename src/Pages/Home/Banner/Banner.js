264fa7a414e6188bcb84178916dc50aef867c939
Nijumdip <nijumdip019911@gmail.com>
2022-09-09 21:16:52 +0600

    edit banner

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,14 +1,32 @@
+import { Carousel } from "flowbite-react";
 import React from "react";
-import "./Banner";
-import Ben2 from "./Ben2";
+import "./Banner.css";
 
 const Banner = () => {
   return (
-    <div className="bg-base-200 my-5">
+    <div className="bg-red-100 my-5">
       <div className="container relative flex flex-wrap justify-around items-center lg:flex-row-reverse py-5">
         
         <div>
-         <Ben2 />
+        <div className="w-80 h-56 sm:h-64 xl:h-80 2xl:h-96">
+        <Carousel>
+          <img
+            className="w-80 h-80"
+            src="https://static.toiimg.com/photo/msid-87930581/87930581.jpg?211826"
+            alt="..."
+          />
+          <img
+            className="w-80 h-80"
+            src="https://templates.hibootstrap.com/restant/default/assets/img/home-one/collection/1.jpg"
+            alt="..."
+          />
+          <img
+            className="w-80 h-80"
+            src="http://www.allbdrecipe.com/Images/Article/Img_2020823_152455_115.jpg"
+            alt="..."
+          />
+        </Carousel>
+      </div>
         </div>
 
         <div className="text-start py-4 px-3 description-div">
@@ -23,7 +41,7 @@ const Banner = () => {
             Get Started
           </button>
         </div>
-        
+
       </div>
     </div>
   );
