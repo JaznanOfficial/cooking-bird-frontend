9400f7d3be99fa59d0af2e8a84bd1e2c489cbff4
Nijumdip <nijumdip019911@gmail.com>
2022-09-10 10:38:19 +0600

    banner complete

--- src/Pages/Home/Banner/BannerCategory.js
+++ src/Pages/Home/Banner/BannerCategory.js
@@ -7,15 +7,14 @@ const BannerCategory = ({category}) => {
     >
       <div>
         <img
-          className="mask w-24 h-24"
-        //   className="mask mask-circle w-24"
+          // className="mask w-24 h-24"
+          className="mask mask-circle w-28 h-24"
           src={category.img}
           alt=""
         />
       </div>
       <div>
-        <h5 className="text-black text-sm mt-5">{category.name}</h5>
-        <h5 className="text-black text-sm ">{category.items}</h5>
+        <h5 className="text-red-600 font-medium mt-1 mb-2">{category.name}</h5>
       </div>
     </div>
   );
