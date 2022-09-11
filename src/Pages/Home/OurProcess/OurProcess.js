eb27c8bdb77bcc706a5d1c54036627ad2babe2e0
Nijumdip <nijumdip019911@gmail.com>
2022-09-11 11:56:21 +0600

    Our Process section alomost done

--- src/Pages/Home/OurProcess/OurProcess.js
+++ src/Pages/Home/OurProcess/OurProcess.js
@@ -2,30 +2,34 @@ import React from "react";
 import OurProcessCard from "./OurProcessCard";
 
 const OurProcess = () => {
-  const ourProcessAll = [
+  const allProcessData = [
     {
       _id: 1,
       name: "Search",
       img: "https://i.ibb.co/dr3Dj6V/download.png",
       description:"Lorem ipsum dlor sit amet, contur adcing elit. Aliam Lorem ipsum dlor si ipsum impedit ullam.",
+      badge:"1"
     },
     {
       _id: 2,
       name: "Select",
       img: "https://i.ibb.co/Wn8y7sx/download-1.png",
       description:"Lorem ipsum dlor sit amet, cotur adcing elit. Aliam Lorem ipsum dlor si ipsum impedit ullam.",
+      badge:"2"
     },
     {
       _id: 3,
       name: "Order",
       img: "https://i.ibb.co/Yy81p3J/download-2.png",
       description:"Lorem ipsum dlor sit amet, cotur asing elit. Aluam Lorem ipsum dlor si ipsum impedit ullam.",
+      badge:"3"
     },
     {
       _id: 4,
       name: "Enjoy",
       img: "https://i.ibb.co/wCnSxxQ/download-3.png",
       description:"Lorem ipsum dlor sit amet, cotur ading elit. Aquam Lorem ipsum dlor si ipsum impedit ulla.",
+      badge:"4"
     },
   ];
 
@@ -36,8 +40,8 @@ const OurProcess = () => {
         <h1 className="text-black text-3xl font-medium">How Does It Work</h1>
       </div>
       <div className="flex flex-wrap justify-center items-center gap-3">
-        {ourProcessAll.map((process) => (
-          <OurProcessCard key={process._id} process={process}></OurProcessCard>
+        {allProcessData?.map((processData) => (
+          <OurProcessCard key={processData._id} processData={processData}></OurProcessCard>
         ))}
       </div>
     </div>
--- src/Pages/Home/OurProcess/OurProcessCard.js
+++ src/Pages/Home/OurProcess/OurProcessCard.js
@@ -1,22 +1,26 @@
+import { Badge } from "flowbite-react";
 import React from "react";
 
-const OurProcessCard = ({ process }) => {
-    const { name, img, description } = process;
+const OurProcessCard = ({ processData }) => {
+  const { name, img, description, badge } = processData;
   return (
-
-        <div className=" w-64 flex flex-col text-center items-center py-5">
-          <div className=" w-28 h-28 bg-red-600 flex flex-col justify-center items-center rounded-full">
-            <img
-              src={img}
-              alt="search"
-              className="w-16 h-16 "
-            />
-          </div>
-          <div className="card-body items-center text-center">
-              <h2 className="card-title">{name}</h2>
-              <p>{description}</p>
-          </div>
+    <div className=" w-64 flex flex-col text-center items-center py-5">
+      <div className="relative w-28 h-28 bg-red-600 flex flex-col justify-center items-center rounded-full">
+        <div className="absolute rounded-full left-0 top-0">
+          <Badge
+            color="purple"
+            size="md"
+          >
+            {badge}
+          </Badge>
+        </div>
+        <img src={img} alt="search" className="w-16 h-16 " />
       </div>
+      <div className="card-body items-center text-center">
+        <h2 className="card-title">{name}</h2>
+        <p>{description}</p>
+      </div>
+    </div>
   );
 };
 
