577d4dc81f29198dc7c12c6abc51b46226fcc47c
JaznanOfficial <jaznanofficial@gmail.com>
2022-09-15 10:39:25 +0600

    menu card done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import HomePage from "./Pages/Home/HomePage/HomePage";
 import Navbar from "./Pages/Home/Navbar/Navbar";
+import MenuPage from "./Pages/MenuPage/MenuPage";
 import SignInPage from "./Pages/SignInPage/SignInPage";
 import SignUpPage from "./Pages/SignUpPage/SignUpPage";
 
@@ -10,7 +11,8 @@ function App() {
   return <div className="App">
     <Navbar/>
     <Routes>
-    <Route path="/" element={<HomePage />}></Route>
+      <Route path="/" element={<HomePage />}></Route>
+      <Route path="/menu" element={<MenuPage />}></Route>
     <Route path="/sign-in" element={<SignInPage/>}></Route>
     <Route path="/sign-up" element={<SignUpPage/>}></Route>
     </Routes>
