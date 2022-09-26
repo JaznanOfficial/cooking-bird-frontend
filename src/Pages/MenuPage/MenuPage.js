6b6d592f25458f38fd4a57e0ca8286793b8bc688
JaznanOfficial <jaznanofficial@gmail.com>
2022-09-26 10:30:29 +0600

    menu page design done

--- src/Pages/MenuPage/MenuPage.js
+++ src/Pages/MenuPage/MenuPage.js
@@ -22,56 +22,53 @@ const MenuPage = () => {
             </div>
 
             <div className="lg:flex justify-center items-start">
-                <div class=" bg-navy-900 p-5 my-3">
-                    <div className=" container flex flex-col justify-center items-center mx-auto">
-                        <h3 className="bg-white inline p-2 rounded-full text-navy-900">
-                            Filter your food as you like...
-                        </h3>
-                        <div class=" ">
-                            <div class="checkbox-container">
-                                <ul class="checkbox-ul inline">
-                                    <li className="mx-2">
-                                        <input type="checkbox" id="checkboxOne" value="dessert" />
-                                        <label for="checkboxOne" className="text-red-600">
-                                            Dessert
-                                        </label>
-                                    </li>
-                                    <li className="mx-2">
-                                        <input type="checkbox" id="checkboxTwo" value="fast food" />
-                                        <label for="checkboxTwo" className="text-red-600">
-                                            Fast Food
-                                        </label>
-                                    </li>
-                                    <li className="mx-2">
-                                        <input
-                                            type="checkbox"
-                                            id="checkboxThree"
-                                            value="breakfast"
-                                        />
-                                        <label for="checkboxThree" className="text-red-600">
-                                            Breakfast
-                                        </label>
-                                    </li>
-                                    <li className="mx-2">
-                                        <input type="checkbox" id="checkboxFour" value="lunch" />
-                                        <label for="checkboxFour" className="text-red-600">
-                                            Lunch
-                                        </label>
-                                    </li>
-                                    <li className="mx-2">
-                                        <input type="checkbox" id="checkboxFive" value="dinner" />
-                                        <label for="checkboxFive" className="text-red-600">
-                                            Dinner
-                                        </label>
-                                    </li>
-                                    <li className="mx-2">
-                                        <input type="checkbox" id="checkboxSix" value="special" />
-                                        <label for="checkboxSix" className="text-red-600">
-                                            Special
-                                        </label>
-                                    </li>
-                                </ul>
-                            </div>
+                <div class=" bg-navy-900 p-5 my-5 lg:w-1/6 sticky top-0">
+                    <div className="container flex flex-col justify-center items-center">
+                        <div className="bg-navy-900 inline rounded-full lg:rounded p-3 mb-2 text-white lg:p-1 lg:mb-0">
+                            <h3>
+                                Filter your food as you like...
+                            </h3>
+                        </div>
+
+                        <div class="checkbox-container">
+                            <ul class="checkbox-ul">
+                                <li>
+                                    <input type="checkbox" id="checkboxOne" value="dessert" />
+                                    <label for="checkboxOne" className="text-red-600">
+                                        Dessert
+                                    </label>
+                                </li>
+                                <li>
+                                    <input type="checkbox" id="checkboxTwo" value="fast food" />
+                                    <label for="checkboxTwo" className="text-red-600">
+                                        Fast Food
+                                    </label>
+                                </li>
+                                <li>
+                                    <input type="checkbox" id="checkboxThree" value="breakfast" />
+                                    <label for="checkboxThree" className="text-red-600">
+                                        Breakfast
+                                    </label>
+                                </li>
+                                <li>
+                                    <input type="checkbox" id="checkboxFour" value="lunch" />
+                                    <label for="checkboxFour" className="text-red-600">
+                                        Lunch
+                                    </label>
+                                </li>
+                                <li>
+                                    <input type="checkbox" id="checkboxFive" value="dinner" />
+                                    <label for="checkboxFive" className="text-red-600">
+                                        Dinner
+                                    </label>
+                                </li>
+                                <li>
+                                    <input type="checkbox" id="checkboxSix" value="special" />
+                                    <label for="checkboxSix" className="text-red-600">
+                                        Special
+                                    </label>
+                                </li>
+                            </ul>
                         </div>
                     </div>
                 </div>
