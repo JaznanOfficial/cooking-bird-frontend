b1968132c0dd5a73cac95535f135aadb9fcc49b8
JaznanOfficial <jaznanofficial@gmail.com>
2022-10-02 08:24:08 +0600

    cart done

--- src/Pages/CartPage/CartPageRight.js
+++ src/Pages/CartPage/CartPageRight.js
@@ -4,74 +4,116 @@ const CartPageRight = () => {
     return (
         <div className="w-full">
             <div className="bg-white p-5 rounded-3xl">
-                <h1 className="text-navy-900 text-start text-5xl">Available Vouchers</h1>
-                <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 container mt-8">
-                    <div className="flex bg-white shadow-lg rounded-3xl mx-4 md:mx-auto my-0 max-w-md md:max-w-2xl border w-full mx-auto">
-                        <div className="flex justify-start items-start px-6 py-6">
-                            <div className="text-start">
-                                <div className="flex items-center justify-between">
-                                    <h2 className="text-lg font-semibold text-gray-900 -mt-1 text-red-600 uppercase bg-orange-100 p-3">
-                                        <i className="fa-solid fa-ticket"></i> eid2023
-                                    </h2>
-                                </div>
+                <h1 className="text-navy-900 text-start text-3xl">Cart</h1>
 
-                                <p className="mt-3 text-gray-700 text-lg">
-                                    Get 50% discount on your first order
-                                </p>
-                                <p className="text-sm">
-                                    use code <strong>EID2023</strong> & get 50% discount on your
-                                    first order on our site. max discount: 200 bdt
-                                </p>
-                                <div className="my-4 flex items-center justify-between">
-                                    <h1 className=" text-gray-700 text-sm">
-                                        will expire: <i className="fa-solid fa-clock"></i>{" "}
-                                        27-09-2022
-                                    </h1>
-                                </div>
-                                <hr />
-                                <div className="flex flex-col lg:flex-row justify-between items-center mt-3">
-                                    <div>
-                                        <button className="bg-red-600 px-2 py-1 rounded-3xl text-gray-100 tracking-wide hover:bg-red-500 ">
-                                            Copy code
+                <div class="overflow-x-none lg:p-3">
+                    <table class="table-auto w-full">
+                        <thead class="text-xs font-semibold uppercase text-gray-400 bg-gray-50">
+                            <tr>
+                                <th class="p-2">
+                                    <div class="font-semibold text-left">Product Name</div>
+                                </th>
+                                <th class="p-2">
+                                    <div class="font-semibold text-left">Quantity</div>
+                                </th>
+                                <th class="p-2">
+                                    <div class="font-semibold text-left">Total</div>
+                                </th>
+                                <th class="p-2">
+                                    <div class="font-semibold text-center">Action</div>
+                                </th>
+                            </tr>
+                        </thead>
+
+                        <tbody class="text-sm divide-y divide-gray-100">
+                            <tr>
+                                <td class="p-2">
+                                    <div class="font-medium text-gray-800 text-start lg:text-xl">
+                                        Samsung Galaxy Note 4
+                                    </div>
+                                </td>
+                                <td class="p-2">
+                                    <div class="text-left flex items-center">
+                                        <button className=" px-2 py-1 bg-white dark:bg-gray-900 hover:bg-red-500 hover:text-white hover:cursor-pointer border border-gray-300 hover:border-white rounded-full">
+                                            <i className="fa-solid fa-minus"></i>
+                                        </button>
+                                        <h1 className="lg:text-xl mx-3">1</h1>
+                                        <button className=" px-2 py-1 bg-white dark:bg-gray-900 hover:bg-red-500 hover:text-white hover:cursor-pointer border border-gray-300 hover:border-white rounded-full">
+                                            <i className="fa-solid fa-plus"></i>
                                         </button>
                                     </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="flex bg-white shadow-lg rounded-3xl mx-4 md:mx-auto my-0 max-w-md md:max-w-2xl border w-full mx-auto">
-                        <div className="flex justify-start items-start px-6 py-6">
-                            <div className="text-start">
-                                <div className="flex items-center justify-between">
-                                    <h2 className="text-lg font-semibold text-gray-900 -mt-1 text-red-600 uppercase bg-orange-100 p-3">
-                                        <i className="fa-solid fa-ticket"></i> eid2023
-                                    </h2>
-                                </div>
-
-                                <p className="mt-3 text-gray-700 text-lg">
-                                    Get 50% discount on your first order
-                                </p>
-                                <p className="text-sm">
-                                    use code <strong>EID2023</strong> & get 50% discount on your
-                                    first order on our site. max discount: 200 bdt
-                                </p>
-                                <div className="my-4 flex items-center justify-between">
-                                    <h1 className=" text-gray-700 text-sm">
-                                        will expire: <i className="fa-solid fa-clock"></i>{" "}
-                                        27-09-2022
-                                    </h1>
-                                </div>
-                                <hr />
-                                <div className="flex flex-col lg:flex-row justify-between items-center mt-3">
-                                    <div>
-                                        <button className="bg-red-600 px-2 py-1 rounded-3xl text-gray-100 tracking-wide hover:bg-red-500 ">
-                                            Copy code
+                                </td>
+                                <td class="p-2">
+                                    <div class="text-left font-medium text-green-500 lg:text-xl">
+                                        RM 2,890.66
+                                    </div>
+                                </td>
+                                <td class="p-2">
+                                    <div class="flex justify-center">
+                                        <button>
+                                            <svg
+                                                class="w-8 h-8 hover:text-red-600 rounded-full hover:bg-gray-100 p-1"
+                                                fill="none"
+                                                stroke="currentColor"
+                                                viewBox="0 0 24 24"
+                                                xmlns="http://www.w3.org/2000/svg"
+                                            >
+                                                <path
+                                                    stroke-linecap="round"
+                                                    stroke-linejoin="round"
+                                                    stroke-width="2"
+                                                    d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+                                                ></path>
+                                            </svg>
                                         </button>
                                     </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td class="p-2">
+                                    <div class="font-medium text-gray-800 text-start lg:text-xl">
+                                        Samsung Galaxy Note 4
+                                    </div>
+                                </td>
+                                <td class="p-2">
+                                    <div class="text-left flex items-center">
+                                        <button className=" px-2 py-1 bg-white dark:bg-gray-900 hover:bg-red-500 hover:text-white hover:cursor-pointer border border-gray-300 hover:border-white rounded-full">
+                                            <i className="fa-solid fa-minus"></i>
+                                        </button>
+                                        <h1 className="lg:text-xl mx-3">1</h1>
+                                        <button className=" px-2 py-1 bg-white dark:bg-gray-900 hover:bg-red-500 hover:text-white hover:cursor-pointer border border-gray-300 hover:border-white rounded-full">
+                                            <i className="fa-solid fa-plus"></i>
+                                        </button>
+                                    </div>
+                                </td>
+                                <td class="p-2">
+                                    <div class="text-left font-medium text-green-500 lg:text-xl">
+                                        RM 2,890.66
+                                    </div>
+                                </td>
+                                <td class="p-2">
+                                    <div class="flex justify-center">
+                                        <button>
+                                            <svg
+                                                class="w-8 h-8 hover:text-red-600 rounded-full hover:bg-gray-100 p-1"
+                                                fill="none"
+                                                stroke="currentColor"
+                                                viewBox="0 0 24 24"
+                                                xmlns="http://www.w3.org/2000/svg"
+                                            >
+                                                <path
+                                                    stroke-linecap="round"
+                                                    stroke-linejoin="round"
+                                                    stroke-width="2"
+                                                    d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+                                                ></path>
+                                            </svg>
+                                        </button>
+                                    </div>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
