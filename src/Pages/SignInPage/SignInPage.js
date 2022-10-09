c0fcdc03faafbcf1226540173150bd903e461b37
Nijumdip <nijumdip019911@gmail.com>
2022-10-09 22:51:43 +0600

    show signUp page input data in console

--- src/Pages/SignInPage/SignInPage.js
+++ src/Pages/SignInPage/SignInPage.js
@@ -1,5 +1,5 @@
-import { Avatar } from "flowbite-react";
 import React , { useRef }from "react";
+import { Avatar } from "flowbite-react";
 import { Link } from "react-router-dom";
 
 const SignInPage = () => {
