9e83824b2d7fb74a9e534b51c60a2d7d62671a4b
JaznanOfficial <jaznanofficial@gmail.com>
2022-10-13 07:49:18 +0600

    checkout folder pattern fixed

--- src/Pages/checkout/Checkout.js
+++ src/Pages/checkout/Checkout.js
@@ -1,10 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import CheckoutAddress from "./CheckoutAddress";
+import CheckoutPayment from "./CheckoutPayment";
 
 const Checkout = () => {
-
-  const background =
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSL6GUpqeY8UWzbMerPMh7wbljDWFZ-zmIlAA&usqp=CAU";
 
   return (
     <div class="py-16 px-5 md:px-6 2xl:px-0 flex justify-center items-center 2xl:mx-auto 2xl:container">
@@ -28,184 +27,15 @@ const Checkout = () => {
             <h1 className="text-navy-900 text-2xl text-start font-ubuntu pt-5">
               Billing details
             </h1>
+           
 
-            <form>
-              <div class="grid grid-cols-1 gap-6 mt-3 md:grid-cols-2">
-                <input
-                  type="text"
-                  placeholder="First Name"
-                  class="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-red-300 dark:focus:border-red-300 focus:ring-red-300 focus:outline-none focus:ring focus:ring-opacity-40"
-                />
-                <input
-                  type="text"
-                  placeholder="Last Name"
-                  class="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-red-300 dark:focus:border-red-300 focus:ring-red-300 focus:outline-none focus:ring focus:ring-opacity-40"
-                />
-
-                <input
-                  type="email"
-                  placeholder="Email"
-                  class="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-red-300 dark:focus:border-red-300 focus:ring-red-300 focus:outline-none focus:ring focus:ring-opacity-40"
-                />
-                <input
-                  type="tel"
-                  placeholder="Phone"
-                  class="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-red-300 dark:focus:border-red-300 focus:ring-red-300 focus:outline-none focus:ring focus:ring-opacity-40"
-                />
-                <input
-                  type="text"
-                  placeholder="Company Name (optional)"
-                  class="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-red-300 dark:focus:border-red-300 focus:ring-red-300 focus:outline-none focus:ring focus:ring-opacity-40"
-                />
-
-                <input
-                  type="text"
-                  placeholder="Country / Region *"
-                  class="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-red-300 dark:focus:border-red-300 focus:ring-red-300 focus:outline-none focus:ring focus:ring-opacity-40"
-                />
-
-                <input
-                  type="text"
-                  placeholder="Town / City *"
-                  class="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-red-300 dark:focus:border-red-300 focus:ring-red-300 focus:outline-none focus:ring focus:ring-opacity-40"
-                />
-
-                <input
-                  type="text"
-                  placeholder="Street Address and House number * "
-                  class="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-red-300 dark:focus:border-red-300 focus:ring-red-300 focus:outline-none focus:ring focus:ring-opacity-40"
-                />
-
-                <input
-                  type="text"
-                  placeholder="Apartment, suite, unit, etc. (optional)"
-                  class="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-red-300 dark:focus:border-red-300 focus:ring-red-300 focus:outline-none focus:ring focus:ring-opacity-40"
-                />
-
-                <input
-                  type="text"
-                  placeholder="Postcode *"
-                  class="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-red-300 dark:focus:border-red-300 focus:ring-red-300 focus:outline-none focus:ring focus:ring-opacity-40"
-                />
-              </div>
-
-              <div>
-                <h1 className="text-navy-900 text-xl text-start font-ubuntu my-3">
-                  Additional information
-                </h1>
-
-                <textarea
-                  name="Order notes (optional)"
-                  cols="30"
-                  rows="5"
-                  class="block w-full px-5  text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-red-300 dark:focus:border-red-300 focus:ring-red-300 focus:outline-none focus:ring focus:ring-opacity-40"
-                ></textarea>
-              </div>
-
-              
-
-              <div>
-                <h1 className="text-navy-900 text-xl text-start font-ubuntu my-3">
-                  Payment method
-                </h1>
-
-                <ul class="p-3 space-y-3 text-sm">
-                  <li>
-                    <div class="flex items-center">
-                      <input
-                        type="radio"
-                        name="default-radio"
-                        class="w-4 h-4"
-                      />
-                      <label
-                        for="default-radio-1"
-                        class="ml-2 text-sm font-medium text-navy-900"
-                      >
-                        Direct bank transfer
-                      </label>
-                    </div>
-                  </li>
-
-                  <li>
-                    <div class="flex items-center">
-                      <input
-                        type="radio"
-                        name="default-radio"
-                        class="w-4 h-4"
-                      />
-                      <label
-                        for="default-radio-1"
-                        class="ml-2 text-sm font-medium text-navy-900"
-                      >
-                        Credit Card
-                      </label>
-                    </div>
-                  </li>
-
-                  <li>
-                    <div class="flex items-center">
-                      <input
-                        type="radio"
-                        name="default-radio"
-                        class="w-4 h-4"
-                      />
-                      <label
-                        for="default-radio-1"
-                        class="ml-2 text-sm font-medium text-navy-900"
-                      >
-                        Check payments
-                      </label>
-                    </div>
-                  </li>
-
-                  <li>
-                    <div class="flex items-center">
-                      <input
-                        type="radio"
-                        name="default-radio"
-                        class="w-4 h-4"
-                      />
-                      <label
-                        for="default-radio-1"
-                        class="ml-2 text-sm font-medium text-navy-900"
-                      >
-                        Cash on delivery
-                      </label>
-                    </div>
-                  </li>
-                </ul>
-              </div>
-
-              <Link
-              to="/"
-              type="submit"
-              className="block w-full  mt-4 py-3 rounded-md text-white font-semibold mb-2 "
-              style={{ background: `url(${background})` }}
-              >
-              Continue to payment
-              </Link>
-              
-              <Link
-                to=""
-                type="submit"
-                className="text-white px-10 block w-full mt-4 py-3 font-medium rounded-lg text-sm mr-2 mb-2 bg-red-600"
-              >
-              Cancel this transfer
-              </Link>
-
-              <Link
-              to="/"
-              type="submit"
-              className="block w-full  mt-4 py-3 rounded-md text-white font-semibold mb-2 "
-              style={{ background: `url(${background})` }}
-              >
-              Cancel this transfer
-              </Link>
-              
-            </form>
+            <CheckoutAddress />
+            <CheckoutAddress />
           </div>
 
-          <div class="xl:w-2/5 flex flex-col sm:flex-row xl:flex-col justify-center items-center bg-gray-100 dark:bg-gray-800 py-7 sm:py-0 xl:py-10 px-10"></div>
+          <div class="xl:w-2/5 flex flex-col sm:flex-row xl:flex-col justify-center items-center bg-gray-100 dark:bg-gray-800 py-7 sm:py-0 xl:py-10 px-10">
+            <CheckoutPayment />
+          </div>
         </div>
       </div>
     </div>
